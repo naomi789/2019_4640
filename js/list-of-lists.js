dc42f5ecf98db8bc2129be617761f40da1528b99
Luke Wolpert <lrw5pu@virginia.edu>
2019-06-20 23:03:44 -0400

    users can create a new list, which will dynamically appear on the displayed list

--- js/list-of-lists.js
+++ js/list-of-lists.js
@@ -5,7 +5,6 @@ function editLists(){
     if(edit_button.innerHTML == "EDIT"){
         edit_button.innerHTML = "DONE";        
         new_list_button.style.display = "flex";
-
     }
     else{
         new_list_button.style.display = "none";
@@ -16,16 +15,54 @@ function editLists(){
 
 
 function newList(){
+    new_list_button = document.getElementById("new-list-button-row");
+    new_list_input = document.getElementById("new-list-input-row");
 
-
-
-
-
-    focus();
+    new_list_button.style.display = "none";
+    new_list_input.style.display = "flex";
 }
 
 
 
 function submitNewList(){
-    
+    new_list_button = document.getElementById("new-list-button-row");
+    new_list_input = document.getElementById("new-list-input-row");
+    name = document.getElementById("list-name-input").value;
+
+    parentDiv = document.createElement("div");
+    parentDiv.setAttribute("class", "row");
+
+    firstChild = document.createElement("div");
+    firstChild.setAttribute("class", "col-0 col-md-2");
+    parentDiv.appendChild(firstChild);
+
+    secondChild = document.createElement("div");
+    secondChild.setAttribute("class", "col-12 col-md-8");
+    secondChild.setAttribute("align", "center");
+
+    grandChild = document.createElement("div");
+    grandChild.setAttribute("class", "box");
+
+    greatGrandChild = document.createElement("span");
+    greatGrandChild.setAttribute("class", "box-txt");
+
+    theText = document.createTextNode(name);
+    greatGrandChild.appendChild(theText);
+
+    grandChild.appendChild(greatGrandChild);
+
+    secondChild.appendChild(grandChild);
+    parentDiv.appendChild(secondChild);
+
+    thirdChild = document.createElement("div");
+    thirdChild.setAttribute("class", "col-0 col-md-2");
+    parentDiv.appendChild(thirdChild);
+
+    containerDiv = (document.getElementsByClassName("container"))[0];
+    containerDiv.appendChild(parentDiv);
+
+    new_list_input.style.display = "none";
+    new_list_button.style.display = "flex";
+
+    console.log(parentDiv);
 }
