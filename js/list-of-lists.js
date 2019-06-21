afd81ac3f811f563e5bca59a63ccd430672d7907
Luke Wolpert <lrw5pu@virginia.edu>
2019-06-21 02:59:05 -0400

    add delete list functionality

--- js/list-of-lists.js
+++ js/list-of-lists.js
@@ -1,3 +1,5 @@
+const hide = element => {element.style.display = "none";}
+
 function editLists(){
     edit_button = document.getElementById("edit-button");
     new_list_button = document.getElementById("new-list-button-row");
@@ -5,11 +7,53 @@ function editLists(){
     if(edit_button.innerHTML == "EDIT"){
         edit_button.innerHTML = "DONE";        
         new_list_button.style.display = "flex";
+
+        secondChildren = document.getElementsByClassName("second-child");
+        for(i=0; i < secondChildren.length; i++){
+            let child = secondChildren[i];
+            child.classList.remove("col-11");
+            child.classList.remove("col-md-8");
+            child.classList.add("col-10");
+            child.classList.add("col-md-7");
+        }
+        thirdChildren = document.getElementsByClassName("third-child");
+        for(i=0; i < thirdChildren.length; i++){
+            let child = thirdChildren[i];
+            child.style.display = "block";
+        }
+        fourthChildren = document.getElementsByClassName("fourth-child");
+        for(i=0; i < fourthChildren.length; i++){
+            let child = fourthChildren[i];
+            child.classList.remove("col-md-2");
+            child.classList.add("col-md-1");
+        }
+
     }
     else{
         hide(new_list_button);
         hide(new_list_input);
         edit_button.innerHTML = "EDIT";
+
+        secondChildren = document.getElementsByClassName("second-child");
+        for(i=0; i < secondChildren.length; i++){
+            let child = secondChildren[i];
+            child.classList.remove("col-10");
+            child.classList.remove("col-md-7");
+            child.classList.add("col-11");
+            child.classList.add("col-md-8");
+        }
+        thirdChildren = document.getElementsByClassName("third-child");
+        for(i=0; i < thirdChildren.length; i++){
+            let child = thirdChildren[i];
+            child.style.display = "none";
+        }
+        fourthChildren = document.getElementsByClassName("fourth-child");
+        for(i=0; i < fourthChildren.length; i++){
+            let child = fourthChildren[i];
+            child.classList.remove("col-md-1");
+            child.classList.add("col-md-2");
+        }
+
     }
 }
 
@@ -30,14 +74,14 @@ function submitNewList(){
     name = document.getElementById("list-name-input").value;
 
     parentDiv = document.createElement("div");
-    parentDiv.setAttribute("class", "row");
+    parentDiv.setAttribute("class", "row list-row");
 
     firstChild = document.createElement("div");
     firstChild.setAttribute("class", "col-0 col-md-2");
     parentDiv.appendChild(firstChild);
 
     secondChild = document.createElement("div");
-    secondChild.setAttribute("class", "col-12 col-md-8");
+    secondChild.setAttribute("class", "second-child col-10 col-md-7");
     secondChild.setAttribute("align", "center");
 
     grandChild = document.createElement("div");
@@ -55,8 +99,22 @@ function submitNewList(){
     parentDiv.appendChild(secondChild);
 
     thirdChild = document.createElement("div");
-    thirdChild.setAttribute("class", "col-0 col-md-2");
+    thirdChild.setAttribute("class", "third-child col-1 col-md-2");
+
+    let button = document.createElement("button");
+    button.setAttribute("class", "delete-button");
+    button.setAttribute("onclick", "deleteList(this)");
+    let icon = document.createElement("i");
+    icon.style.cssText = "vertical-align: middle;"
+    icon.setAttribute("class", "fa fa-minus-circle");
+    button.appendChild(icon);
+    thirdChild.appendChild(button);
+    thirdChild.style.display = "flex";
     parentDiv.appendChild(thirdChild);
+
+    fourthChild = document.createElement("div");
+    fourthChild.setAttribute("class", "fourth-child col-1 col-md-1");
+    parentDiv.appendChild(fourthChild);
 
     containerDiv = (document.getElementsByClassName("container"))[0];
     containerDiv.appendChild(parentDiv);
@@ -66,4 +124,10 @@ function submitNewList(){
 
 }
 
-const hide = element => {element.style.display = "none";}
+function deleteList(element){
+    let parent = element.parentElement;
+    let grandparent = parent.parentElement;
+    grandparent.remove();
+}
+
+
