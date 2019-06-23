c62d88fc5ce53e2cbba0b62f49a6d6bc87cf7bdb
naomi <snjohnson789@gmail.com>
2019-06-23 17:27:14 -0400

    adding php file with data on db connection

---
+++ php/db.inc.php
@@ -0,0 +1,8 @@
+<?php
+$dbServername = "localhost";
+$dbUsername = "data";
+$dbPassword = "password";
+$dbName = "main_db";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+?>
