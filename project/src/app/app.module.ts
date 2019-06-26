0f221b078240cecdd963a8884b0d74808064cb66
Luke Wolpert <lrw5pu@virginia.edu>
2019-06-26 18:26:17 -0400

    cleaned up angular project and created skeleton for form component

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,24 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { SearchResultsComponent } from './search-results/search-results.component';
-import { ListOfListsComponent } from './list-of-lists/list-of-lists.component';
-import { ListComponent } from './list/list.component';
-import { CardComponent } from './card/card.component';
-import { QuizComponent } from './quiz/quiz.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    SearchResultsComponent,
-    ListOfListsComponent,
-    ListComponent,
-    CardComponent,
-    QuizComponent
+    SignupComponent
   ],
   imports: [
     BrowserModule,
--- project/src/app/signup/signup.component.spec.ts
+++ project/src/app/signup/signup.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { SignupComponent } from './signup.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('SignupComponent', () => {
+  let component: SignupComponent;
+  let fixture: ComponentFixture<SignupComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ SignupComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(SignupComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
