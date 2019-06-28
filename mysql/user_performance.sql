ce9bcbfebf0a1d59e084b6e008d97a5f2485d6ce
naomi <snjohnson789@gmail.com>
2019-06-28 12:49:21 -0400

    added user_performance table stuff

---
+++ mysql/user_performance.sql
@@ -0,0 +1,15 @@
+USE main_db;
+
+CREATE TABLE IF NOT EXISTS user_performance(
+	time_date NVARCHAR(100) NOT NULL,
+	time_thinking NVARCHAR(100) NOT NULL,
+	list_name NVARCHAR(100),
+	correct BOOLEAN NOT NULL,
+	ent_seq INT NOT NULL,
+	username NVARCHAR(100),
+	CONSTRAINT PK_JMdict PRIMARY KEY (ent_seq, list_name, username)
+);
+
+ALTER TABLE user_performance CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE user_performance ADD FOREIGN KEY (ent_seq) REFERENCES JMdict(ent_seq);
