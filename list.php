df85f8da05c745cda0618e51c0a15616633b6d22
naomi <snjohnson789@gmail.com>
2019-06-28 10:28:18 -0400

    trying to get sessions working

--- list.php
+++ list.php
@@ -21,7 +21,7 @@
             <div class="header">
                   <a href="/" class="logo"><img class="photo" src="images/logo.PNG" alt="JDict Japanese English Dictionary" height="100px"></a>
                   <div class="header-right">
-                    <a href="list-of-lists.html">My vocab lists</a>
+                    <a href="list-of-lists.php">My vocab lists</a>
 
                   </div>
             </div>
@@ -32,7 +32,7 @@
 		<!--Row 2: List Name-->
 		<div class="row">
 			<div class="col-0 col-md-2"></div>
-			<div class="col-5 col-md-4" id="list-name">TOEFL Exam Prep</div>
+			<div class="col-5 col-md-4" id="list-name"><?PHP $all_words ?></div>
 			<div class="col-7 col-md-6"></div>
 		</div>
 
@@ -41,7 +41,7 @@
 		<div class="row">
 			<div class="col-0 col-md-4"></div>
 			<div class="col-12 col-md-4 flip-container" ontouchstart="this.classList.toggle('hover');">
-				<div align="center" class="card" id="flashcard" onclick="clickCard()">
+				<div align="center" class="card" id="flashcard">
 				  <!-- front content -->
 				  難しい
 				</div>
@@ -56,11 +56,11 @@
 			<div class="col-4 col-md-5"></div>
 <!-- 			<div class="col-3 col-md-1 left-arrow"><button class="mini-flash-btn" onclick="goLeft(this)"><i class="fa fa-angle-left"></button></i></div>
 			<div class="col-1 right-arrow"><button class="mini-flash-btn" onclick="goRight(this)"><i class="fa fa-angle-right"></button></i></div> -->
-      <form method="post" action="">
+      <form method="get" action="">
 <!--         <input type="search" name="value" id="search_box" placeholder="Type a word in English or Japanese.." autofocus="autofocus">
         <button type="submit" onclick="search()"><i class="fa fa-search"></i></button> -->
-        <div class="col-3 col-md-1 left-arrow"><button class="mini-flash-btn" name="next" onclick="goLeft(this)"><i class="fa fa-angle-left"></button></i></div>
-        <div class="col-1 right-arrow"><button class="mini-flash-btn" name="previous" onclick="goRight(this)"><i class="fa fa-angle-right"></button></i></div>
+        <div class="col-3 col-md-1 left-arrow"><button class="mini-flash-btn" name="previous_word"><i class="fa fa-angle-left"></button></i></div>
+        <div class="col-1 right-arrow"><button class="mini-flash-btn" name="next_word"><i class="fa fa-angle-right"></button></i></div>
       </form>
 
 
@@ -79,64 +79,88 @@
 
 
 
-		<!--Row 6: Edit button-->
-		<div class="row">
+		<!--Row 6: Edit button (for another day)-->
+<!-- 		<div class="row">
 			<div class="col-11 col-md-9"></div>
 			<div class="col-1 col-md-3" id="edit-lists-div"><a class="edit-link" href="503-service-unavailable.html">EDIT</a></div>
-		</div>
+		</div> -->
 		<!--Row 7-->
 		<br />
 
     <?php
-        if ($_SERVER['REQUEST_METHOD'] === 'GET')
+    session_start();
+    $all_words = [];
+    if ($_SERVER['REQUEST_METHOD'] === 'GET')
+    {
+      require('connect-db.php');
+
+      if (isset($_GET['listname']))
+      {
+        $somequery = $_GET['listname'];
+
+        $query = "SELECT * FROM words_in_lists WHERE list_name = " . "'" . $somequery . "' ORDER BY gloss_def;";
+
+        $statement = $db->prepare($query);
+        $statement->execute();
+        $all_words = $statement->fetchAll();
+        $_SESSION['all_words'] = $all_words; 
+        $statement->closecursor();
+      }
+      else
+      {
+        $all_words = $_SESSION['all_words'];
+      }
+      if (count($all_words) == 0)
         {
-          require('connect-db.php');
-          $somequery = $_GET['listname'];
-
-          $query = "SELECT * FROM words_in_lists WHERE list_name = " . "'" . $somequery . "'";
-
-          $statement = $db->prepare($query);
-          $statement->execute();
-          $results = $statement->fetchAll();
-          $statement->closecursor();
-          if (count($results) == 0)
+          echo '<div class="row">
+                            <div class="col-0 col-md-3"></div>
+                            <div class="col-12 col-md-6" align="center">
+                                <p>Oh no! It seems like this vocab list is empty!!</p>
+                            </div>
+                            <div class="col-0 col-md-3"></div>
+                    </div>';
+        }
+        else
+        {
+          echo '<table><tbody>';
           {
+          foreach ($all_words as $one_word)
             echo '<div class="row">
-                              <div class="col-0 col-md-3"></div>
-                              <div class="col-12 col-md-6" align="center">
-                                  <p>Oh no! It seems like this vocab list is empty!!</p>
-                              </div>
-                              <div class="col-0 col-md-3"></div>
-                      </div>';
+                            <div class="col-0 col-md-3"></div>
+                            <div class="col-12 col-md-6" align="center">
+                                <div class="box"><span class="box-txt">'. $one_word['gloss_def'] . '</span></div>
+                            </div>
+                            <div class="col-0 col-md-3"></div>
+                    </div>';
           }
-          else
-          {
-            echo '<table><tbody>';
-            foreach ($results as $result)
-            {
-              echo '<div class="row">
-                              <div class="col-0 col-md-3"></div>
-                              <div class="col-12 col-md-6" align="center">
-                                  <div class="box"><span class="box-txt">'. $result['gloss_def'] . '</span></div>
-                              </div>
-                              <div class="col-0 col-md-3"></div>
-                      </div>';
-            }
-            echo '</tbody></table>';
-          }
+          echo '</tbody></table>';
         }
-    ?>
-    <? PHP
-      function next()
-      {
+      
+    }
 
-        }
-      }
-      function previous()
+    // decide if we need to show next/previous card
+    if ($_SERVER['REQUEST_METHOD'] === 'GET')
+    {
+      echo 'test';
+      if (isset($_GET['next_word']))
       {
-
+        next_word($all_words);
       }
-    ?>
+      if (isset($_GET['previous_word']))
+      {
+        previous_word($all_words);
+      }
+    }   
+
+    function next_word()
+    {
+      echo 'NEXT';
+    }
+    function previous_word()
+    {
+      echo 'PREVIOUS';
+    }
+  ?>
 
 </div>
 
