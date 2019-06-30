fcd61d1c5364bfa30f7f7daeef997f07345fb394
Luke Wolpert <lrw5pu@virginia.edu>
2019-06-30 01:01:15 -0400

    users are given error if they try to create account with an already registered email address

--- project/src/app/signup/signup.component.ts
+++ project/src/app/signup/signup.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { SignUpInfo } from '../sign-up-info';
 import{ HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';  //the first one is most important; the second two provide additional abilities in case we need special treatment
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -27,6 +28,15 @@ export class SignupComponent implements OnInit {
     this.http.post<SignUpInfo>('http://localhost/2019_4640/project/src/app/ngphp-post.php', data).subscribe((data) => {
       console.log('Got data from backend');
       this.newUser = data;
+      if (this.newUser.name == 'userAlreadyExists'){
+        //let theForm = this.newUser.signUpForm;
+        let theForm = document.getElementById("signUpForm");
+        let errorMessage = document.createTextNode("An account is already registered with this email address. Please <a href='sign-in.php'>sign in</a>.");
+        let errorElement = document.createElement("p");
+        errorElement.appendChild(errorMessage);
+        errorElement.setAttribute("class", "alert alert-danger");
+        theForm.appendChild(errorElement);  
+      }
       console.log(this.newUser);
     }, (error)=> {
       console.log('Error', error);
