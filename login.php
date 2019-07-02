356e517d9370fe871d75f1032746817b6ff5f7cf
Luke Wolpert <lrw5pu@virginia.edu>
2019-07-01 20:45:45 -0400

    styled sign in page

--- login.php
+++ login.php
@@ -3,9 +3,13 @@
 
 <head>
 <meta charset="utf-8">
-  <title>Log in</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <title>Log in</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css" />
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
 </head>
 
 
