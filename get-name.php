aa97078f8ec8f18e15344f52fdff13466fa881f0
naomi <snjohnson789@gmail.com>
2019-07-04 18:29:49 -0400

    accessed email from cookie instead of get request

--- get-name.php
+++ get-name.php
@@ -2,19 +2,18 @@
 
 require('connect-db.php');
 
-$email = $_GET['email'];
-
-
-
 function makeGreeting(){
     global $email;
     global $db;
+
+    $email = $_COOKIE['email'];
 
     $query = "SELECT first_name FROM user WHERE email='$email';";
     $statement = $db->prepare($query);
     $statement->execute();
     $results = $statement->fetchAll();
     $statement->closecursor();
+    // echo $alt_email . 'is your email';
     $str = "Hello, " . $results[0]['first_name'];
     echo $str;
 
